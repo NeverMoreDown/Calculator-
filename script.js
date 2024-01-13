9fa2ebf63c004c14407fc23fde7a045cc7b4605b
Jakob Paddy <jakobpaddy@gmail.com>
2024-01-13 04:06:10 -0700

    Add inital files and operator functions in js

---
+++ script.js
@@ -0,0 +1,27 @@
+/*create functions for add subtract multiply and divide 
+These functions need to take two Items 
+Num: 1 and Num: 2 */
+
+function add (num1,num2) {
+    return num1 + num2; 
+}
+
+function subtract (num1,num2) {
+    return num1 - num2;
+}
+
+function multiply (num1,num2) {
+    return num1 * num2;
+}
+
+function divide (num1,num2) {
+    return num1 / num2;
+}
+
+/*Operate function takes a string ex. '3 + 5' turns it into an array ex.['3','+','5'] with three parts
+Num1 an Operator and Num2. Depending the operator it runs a function to the coresponding
+operator*/
+
+function operator (string) {
+    string.split('')
+}
